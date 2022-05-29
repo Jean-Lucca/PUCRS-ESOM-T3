8ec9686b7c9e03965de82973f2518fb5326c4f66
Jean Lucca <42757491+Jean-Lucca@users.noreply.github.com>
2022-05-29 19:02:26 -0300

    Update RestApiApplicationTests.java

--- src/test/java/com/project/restapi/RestApiApplicationTests.java
+++ src/test/java/com/project/restapi/RestApiApplicationTests.java
@@ -1,13 +1,39 @@
 package com.project.restapi;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.project.restapi.entity.Cliente;
+import com.project.restapi.entity.Endereco;
+import com.project.restapi.repository.ClienteRepository;
+import com.project.restapi.repository.EnderecoRepository;
+import com.project.restapi.service.CepService;
+import com.project.restapi.service.impl.ClienteServiceImpl;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RestApiApplicationTests {
 
+	@Autowired
+	ClienteRepository clienteRepository;
+	@Autowired
+	EnderecoRepository enderecoRepository;
+	@Autowired
+	CepService CepService;
 	@Test
-	void contextLoads() {
+	public void testCreate() {
+		Cliente cliente = new Cliente();
+		clienteRepository.save(cliente);
+		assertNotNull(clienteRepository.findById((long) 1));
+	}
+
+	@Test
+	public void testViaCEP() {
+		Endereco endereco = CepService.consultarCep("90020180");
+		assertEquals("Centro Histórico", endereco.getBairro());
 	}
 
 }
